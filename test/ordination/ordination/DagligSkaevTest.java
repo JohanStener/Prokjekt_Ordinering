b9492a3765888a0e173052ec839c75170be8101a
JohanStener <eaajojs@students.eaaa.dk>
2022-09-23 09:51:59 +0200

    Test

---
+++ test/ordination/ordination/DagligSkaevTest.java
@@ -0,0 +1,18 @@
+package ordination.ordination;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DagligSkaevTest {
+
+    @org.junit.jupiter.api.Test
+    void opretDosis() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void samletDosis() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void doegnDosis() {
+    }
+}
---
+++ test/ordination/ordination/PNTest.java
@@ -0,0 +1,22 @@
+package ordination.ordination;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PNTest {
+
+    @org.junit.jupiter.api.Test
+    void givDosis() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void doegnDosis() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getType() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void samletDosis() {
+    }
+}
